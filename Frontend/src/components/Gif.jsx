3a3141a21475a348b4e6e50cfca56114aeafe5d0
VlaCas <34453102+VlaCas@users.noreply.github.com>
2023-07-26 13:22:03 -0400

    actualizacion

--- Frontend/src/components/Gif.jsx
+++
@@ -1,9 +0,0 @@
-function Gif() {
-  return ( 
-    <>
-      <img src="C:/Users/Administrador/Documents/GitHub/final_project/Frontend/src/assets/images/image.jpg" alt=""/>
-    </>
-  );
-};
-
-export default Gif;
