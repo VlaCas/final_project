b36c2a88cdc17d57211407a56f17b155b9d05e20
VlaCas <34453102+VlaCas@users.noreply.github.com>
2023-08-30 11:22:25 -0400

    update

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -22,19 +22,19 @@ function Login() {
   }, [isAuthenticated, errors]);
 
 	return (
-		<section className='bg-black flex flex-col items-center justify-center sectionRegister px-5 py-4 lg:flex-row'>
+		<section className='flex flex-col items-center justify-center px-5 py-4 bg-black sectionRegister lg:flex-row'>
 			<section className='text-white flex flex-col items-center h-full justify-center gap-16 w-full sm:w-full md:w-[55%]'>
 				<div className='flex flex-col gap-10 w-4/5 sm:w-[55%] md:w-full lg:w-4/5 xl:w-4/6'>
 					<h1 className='text-white title-register md:text-6xl lg:pt-0'>Bienvenido</h1>
 					<form className='flex flex-col gap-6 text-lg font-medium' onSubmit={onSubmit}>
 						<div className='flex flex-col w-full gap-2'>
 							<label htmlFor='email' className='text-[#AFAFAF]'>Email</label>
-							<input type='text' {...register('email', { required: { value: true, message: 'Por favor, debe llenar todos los campos.' }, pattern: { value: /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/, message: 'Introduzca un correo válido.' } })} placeholder='ejemplo@gmail.com' className='w-full p-2 border border-solid border-[#ffffff0d] rounded-lg outline-none bg-transparent placeholder-[#555555]'	onChange={() => {}} onKeyDown={(e) => {if (e.key === 'Enter') {e.preventDefault(); onSubmit(); setClickSubmit((current) => !current)}}}/>
+							<input type='text' {...register('email', { required: { value: true, message: 'Por favor, introduzca su correo.' }, pattern: { value: /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/, message: 'Introduzca un correo válido.' } })} placeholder='ejemplo@gmail.com' className='w-full p-2 border border-solid border-[#ffffff0d] rounded-lg outline-none bg-transparent placeholder-[#555555]'	onChange={() => {}} onKeyDown={(e) => {if (e.key === 'Enter') {e.preventDefault(); onSubmit(); setClickSubmit((current) => !current)}}}/>
 						</div>
 						<div className='flex flex-col gap-6'>
 							<div className='flex flex-col gap-2'>
 								<label htmlFor='contraseña' className='text-[#AFAFAF]'>Contraseña</label>
-								<input type='password' {...register('password', { required: { value: true, message: 'Por favor, debe llenar todos los campos.' }, pattern: { value: '^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]*$', message: 'Contraseña incorrecta' } })} placeholder='Ingrese su contraseña' className='w-full p-2 border border-solid border-[#ffffff0d] rounded-lg outline-none bg-transparent placeholder-[#555555]' onChange={() => {}} onKeyDown={(e) => {if (e.key === 'Enter') {e.preventDefault(); onSubmit(); setClickSubmit((current) => !current)}}}/>
+								<input type='password' {...register('password', { required: { value: true, message: 'Por favor, introduzca su contraseña.' }, pattern: { value: '^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]*$', message: 'Contraseña incorrecta' } })} placeholder='Ingrese su contraseña' className='w-full p-2 border border-solid border-[#ffffff0d] rounded-lg outline-none bg-transparent placeholder-[#555555]' onChange={() => {}} onKeyDown={(e) => {if (e.key === 'Enter') {e.preventDefault(); onSubmit(); setClickSubmit((current) => !current)}}}/>
 							</div>
 						</div>
 						<div className='w-full'>
